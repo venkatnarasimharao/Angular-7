80b0fdafa84dfbfb0220b0cbf612c9cc1c3c6720
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-12-15 18:41:47 +0530

    push

---
+++ JSON_Exampl3/json.js
@@ -0,0 +1,12 @@
+/*
+var data:any={
+    orcle:():string=>{
+        return "oracle connection soon....!";
+    },
+    mysql:():string=>{
+        return "mysql connection soon.....!";
+    },
+
+};
+document.write(data.orcle()+"<br>"+data.mysql());
+*/
