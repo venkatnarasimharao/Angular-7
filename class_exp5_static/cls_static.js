c7c33d203c899f9a2a6c14cbf11340214786a3e2
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-12-19 22:06:56 +0530

    push

---
+++ class_exp5_static/cls_static.js
@@ -0,0 +1,23 @@
+var cla_one = /** @class */ (function () {
+    function cla_one() {
+    }
+    cla_one.myFun = function () {
+        return this.var_one;
+    };
+    ;
+    cla_one.var_one = "Narasimha";
+    return cla_one;
+}());
+;
+document.write(cla_one.var_one + "<br>" + cla_one.myFun());
+/*
+ for static keyword no need to create a class
+ static method don't required any object
+ we can access static variable with class name
+by static keyword varibale is created in class ,not in heap memory
+if we have a common variable with same value( var_one ="employee") for every classes if we give in each and every
+class it makes memory for each class ,bcz of this memory waste, by this keyword it creates in class memory.
+
+static can't initilize by using constructor.
+
+*/ 
