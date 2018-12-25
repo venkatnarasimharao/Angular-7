339a9e8d653f6523443de63470f07c2b6ffc5a53
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-12-26 00:50:30 +0530

    pop

--- class_exp4_readonly/class.ts
+++
@@ -1,8 +0,0 @@
-class cls_one{
-    readonly var_one:string;
-    constructor(public arg1:string){
-        this.var_one=arg1;
-    };
-};
-var obj:cls_one=new cls_one("Narasimha");
-document.write( obj.var_one);
